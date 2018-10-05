1538ffd4544991261dd39a2124267e4e3fe1312f
S J Palmer <samuelpalmer94@gmail.com>
2018-10-06 00:00:39 +0100

    Improve test to check P3 boundary conditions at every interface in a multilayer system

--- test/test_P3_BCs.jl
+++ test/test_P3_BCs.jl
@@ -1,29 +1,36 @@
 α = TransferMatrixMethod.α
 
-d_list = [0, 0]
-epr_list = 1 .+ rand(ComplexF64, 2)
-mur_list = 1 .+ rand(ComplexF64, 2)
+k0 = 2pi/1
+d_list = vcat(0, rand(10), 0)
+epr_list = 1 .+ rand(ComplexF64, length(d_list))
+mur_list = 1 .+ rand(ComplexF64, length(d_list))
+P3_list = rand([0,1/2], length(d_list))
 
-for P3_list in ([0,0], [0,1/2], [1/2,0])
-    g = Geometry(d_list, epr_list, mur_list, P3_list)
-    res = tmm(g, k0, pi/3*rand(), pi/2*rand())
-    E1x = res.inc.Ex + res.ref.Ex
-    E1y = res.inc.Ey + res.ref.Ey
-    E1z = res.inc.Ez + res.ref.Ez
-    H1x = res.inc.Hx + res.ref.Hx
-    H1y = res.inc.Hy + res.ref.Hy
-    H1z = res.inc.Hz + res.ref.Hz
-    E2x = res.trn.Ex
-    E2y = res.trn.Ey
-    E2z = res.trn.Ez
-    H2x = res.trn.Hx
-    H2y = res.trn.Hy
-    H2z = res.trn.Hz
-    ΔP3 = P3_list[2] - P3_list[1]
+g = Geometry(d_list, epr_list, mur_list, P3_list)
+res = tmm(g, k0, pi/3*rand(), pi/2*rand())
+
+z_list = cumsum(d_list)
+for (layer,z) in enumerate(z_list[1:end-1])
+    xs = -5:5
+    zs = [z-1e-15, z+1e-15]
+    E, H = get_fields(res, xs, zs)
+    E1x = E[:,1,1]
+    E1y = E[:,1,2]
+    E1z = E[:,1,3]
+    H1x = H[:,1,1]
+    H1y = H[:,1,2]
+    H1z = H[:,1,3]
+    E2x = E[:,2,1]
+    E2y = E[:,2,2]
+    E2z = E[:,2,3]
+    H2x = H[:,2,1]
+    H2y = H[:,2,2]
+    H2z = H[:,2,3]
+    ΔP3 = P3_list[layer+1] - P3_list[layer]
     @test isapprox(E1x, E2x)
     @test isapprox(E1y, E2y)
-    @test isapprox(epr_list[1]*E1z-2im*α*ΔP3*mur_list[1]*H1z, epr_list[2]*E2z) # this BC was not programmed into the TMM at all :)
+    @test isapprox(epr_list[layer].*E1z.-2im*α*ΔP3*mur_list[layer].*H1z, epr_list[layer+1].*E2z) # this BC was not programmed into the TMM at all :)
     @test isapprox(H1x-2im*α*ΔP3*E1x, H2x)
     @test isapprox(H1y-2im*α*ΔP3*E1y, H2y)
-    @test isapprox(mur_list[1]*H1z, mur_list[2]*H2z)
+    @test isapprox(mur_list[layer]*H1z, mur_list[layer+1]*H2z)
 end
